2959c9fc60f0d2a39cf372b27cd2bc019c9c0b5e
Matheus <Matheus@MATHEUS>
2017-08-08 21:37:16 -0300

    teste 3

---
+++ Database.sql
@@ -0,0 +1,386 @@
+﻿/* Create Database db_envioFacil Encoding = 'utf8' Template0; */
+
+
+Create Table configuracao
+(
+	id              Serial Unique Not Null
+	, ativo         SmallInt Not Null Default 1
+	, nome          Varchar(100) Not Null
+	, valor         Text Not Null
+	, dataCadastro  TimeStamp Not Null Default Now()
+	, dataAlteracao TimeStamp
+	, Constraint pk_configuracao_id Primary Key(id)
+)With Oids;
+
+
+
+Create Table pessoa
+(
+	id                      Serial Unique Not Null
+	, ativo                 SmallInt Not Null Default 1
+	, idCliente             Int Default Null	
+	, nome                  Varchar(100)
+	, primeiroNome          Varchar(20) Not Null
+	, sobreNome             Varchar(80)
+	, razaoSocial           Varchar(100)
+	, nomeFantasia          Varchar(100)
+	, sexo                  Varchar(1) Default Null
+	, email                 Varchar(60) Not Null
+	, tipoPessoa            Char(1)	
+	, rg                    Varchar(12)
+	, inscricaoEstadual     Varchar(16)
+	, inscricaoMunicipal    Varchar(16)
+	, cpf                   Varchar(11)
+	, cnpj                  Varchar(14)
+	, dataNascimento        TimeStamp Default Null
+	, usuarioSistema        SmallInt Not Null Default 0
+	, dataCadastro          TimeStamp Default Now()
+	, dataAtualizacao       TimeStamp Default Now()
+	, Constraint pk_pessoa_id Primary Key (id)
+	, Constraint ck_pessoa_tipoPessoa Check (TipoPessoa IN ('F', 'J'))
+) With Oids;
+Comment On Column pessoa.usuarioSistema Is 'Flag que indica se trata-se um usuário do sistema. 1=Sim; 2=Não, ou seja é apenas um cliente dos nossos clientes';
+Create Index idx_pessoa_nome_login_senha On pessoa(nome, login, senha);
+
+
+
+Create Table cliente
+(
+	id                   Serial Not Null Unique
+	, ativo              SmallInt Not Null Default 1
+	, idPessoa           Int Not Null Unique	
+	, licencaLiberada    SmallInt Not Null Default 1
+	, login              Varchar(40)
+	, senha              Varchar(20)
+	, avisoSistema       Varchar(80) Default Null
+	, dataCadastro       TimeStamp Default Now()
+	, dataAtualizacao    TimeStamp Default Now()
+	, chaveCripto        Varchar(32) Not Null
+	, Constraint pk_cliente_id Primary Key (id)
+	, Foreign Key (idPessoa) References pessoa(id)
+)With Oids;
+Comment On Column cliente.avisoSistema Is 'Mensagem que será exibida na tela do aplicativo. Serve para casos em que temos que enviar alguma mensagem aos usuários.';
+Alter Table pessoa Add Constraint fk_cliente_idPessoa Foreign key (idCliente) References cliente(id) Match Simple;
+
+
+Create Table configuracao_cliente
+(
+	id              Serial Unique Not Null
+	, ativo         SmallInt Not Null Default 1
+	, idCliente     Int Not Null
+	, nome          Varchar(100) Not Null
+	, valor         Text Not Null
+	, dataCadastro  TimeStamp Not Null Default Now()
+	, dataAlteracao TimeStamp
+	, Constraint pk_configuracao_cliente_id Primary Key(id)
+	, Foreign key (idCliente) References cliente(id)
+)With Oids;
+
+
+Create Table operadora
+(
+	id             Serial Unique Not NUll
+	, ativo        SmallInt Not Null Default 1
+	, nome         Varchar(40) Not Null
+	, Descricao    Text
+	, dataCadastro TimeStamp Not Null Default Now()
+	, Constraint pk_operadora_id Primary key (id)
+)With Oids;
+
+
+
+Create Table tipo_telefone
+(
+	id              Serial Unique Not Null
+	, ativo         SmallInt Default 1
+	, descricao     Varchar(40) Not Null
+	, dataCadastro  TimeStamp Default Now()
+	, Constraint pk_tipo_telefone_id Primary Key(id)
+)With Oids;
+
+
+Create Table pessoa_telefone
+(
+	id                  Serial Unique Not Null
+	, ativo             SmallInt Not Null Default 1
+	, idPessoa          Int Not Null
+	, idTipoTelefone    Int Not Null
+	, idOperadora       Int Default Null
+	, ddi               Varchar(3) Not Null
+	, ddd               Varchar(3) Not Null
+	, numero            Varchar(10) Not Null
+	, observacao        Varchar(1024) Default Null
+	, Constraint pk_pessoa_telefone_id Primary Key(id)
+	, Foreign Key (idPessoa) References pessoa(id)
+	, Foreign Key (idTipoTelefone) References tipo_telefone(id)
+)With Oids;
+Create Index idx_pessoa_telefone_idPessoa On pessoa_telefone(idPessoa);
+
+
+Create Table forma_pagamento
+(
+	id                       Serial Unique Not Null
+	, ativo                  SmallInt Not Null Default 1
+	, descricao              Varchar(40) Not Null
+	, numeroMaximoParcelas   Int Default Null
+)With Oids;
+
+
+Create Table venda
+(
+	id                   Serial Unique Not Null
+	, idPessoa           Int Not Null
+	, idFormaPagamento   Int Not Null
+	, vlTotal            Real Not Null
+	, vlDesconto         Real Not Null
+	, vlRecebido         Real Default Null
+	, excluida           SmallInt Not Null Default 0
+	, cancelada          SmallInt Not Null Default 0
+	, dataCadastro       TimeStamp Not Null Default Now()
+	, dataFinalizacao    TimeStamp Default Null
+	, dataRetorno        TimeStamp Default Null
+	, dataCancelamento   TimeStamp Default Null
+	, Constraint pk_venda_id Primary Key (id)
+	, Foreign Key (idPessoa) References pessoa(id)
+	, Foreign Key (idFormaPagamento) References forma_pagamento(id)
+)With Oids;
+Comment On Column venda.dataCadastro Is 'Data na qual o registro foi criado no banco de dados (usuário iniciou uma venda)';
+Comment On Column venda.dataFinalizacao Is 'Data que o usuário confirmou a venda';
+Comment On Column venda.dataRetorno Is 'Data de retorno (para saber se a venda foi aprovada, negada, etc.)';
+Comment On Column venda.dataCancelamento Is 'Data de cancelamento da venda';
+Create Index idx_venda_dataFinalizacao On venda(dataFinalizacao);
+
+
+Create Table item_venda
+(
+	id              Serial Unique Not Null
+	, idVenda       Int Not Null
+	, idProduto     Int Not Null
+	, vlUnitario    Real Not Null
+	, quantidade    Real Not Null
+	, vlTotal       Real Not Null
+	, excluido      SmallInt Default 0
+	, dataCadastro  TimeStamp Not Null Default Now()
+	, dataExclusao  TimeStamp Default Null
+	, Constraint pk_item_venda_id Primary Key (id)
+	, Foreign Key (idVenda) References venda(id)
+)With Oids;
+
+
+Create Table tipo_movimento
+(
+	id              Serial Unique Not Null
+	, ativo         SmallInt Not Null Default 1
+	, descricao     Varchar(40)
+	, Constraint pk_tipo_movimento_id Primary Key (id)
+)With Oids;
+
+
+Create Table produto
+(
+	id                  Serial Unique Not Null
+	, ativo             SmallInt Not Null Default 1
+	, nome              Varchar(80) Not Null
+	, descricao         Text Default Null
+	, codigoVendavel    varchar(32) Not Null
+	, estoque           Real Not Null
+	, vlCustoUnitario   Real Not Null
+	, vlVendaUnitario   Real Not Null	
+	, Constraint pk_compra_credito_id Primary Key(id)
+)With Oids;
+
+
+Alter Table item_venda Add Constraint fk_item_venda_idProduto Foreign Key (idProduto) References produto(id);
+
+
+Create Table produto_movimento
+(
+	id                 Serial Unique Not Null
+	, idProduto        Int Not Null
+	, idTipoMovimento  Int Not Null
+	, idVenda          Int
+	, qtdEntrada       Real Default Null
+	, qtdSaida         Real Default Null
+	, qtdFinal         Real Not Null
+	, qtdAnterior      Real Not Null	
+	, Constraint pk_produto_movimento_id Primary Key(id)
+	, Foreign Key (idProduto) References produto(id)
+	, Foreign Key (idTipoMovimento) References tipo_movimento(id)
+)With Oids;
+
+/*
+Create Or Replace Function f_trg_produto_calcular_valor_total()
+	Returns Trigger As $trg_ai_produto_calcular_vlTotal$
+Begin
+	If (Coalesce(New.vlTotal, 0) = 0) Then
+		New.vlTotal = New.vlUnitario * New.quantidade;
+	End If;
+
+	Return Null;
+End;
+$trg_ai_produto_calcular_vlTotal$ Language plpgsql;
+
+
+Create Trigger trg_ai_produto_calcular_vlTotal
+	After Insert Or Update
+	On produto
+	For Each Row 
+		Execute Procedure f_trg_credito_venda_calcular_valor_total();
+*/
+
+
+Create Table produto_plano_venda
+(
+	id             Serial Unique Not Null
+	, ativo        SmallInt Not Null Default 1
+	, idProduto    Int Not Null
+	, descricao    Varchar(80) Not Null
+	, observacao   Text
+	, quantidade   Real Not Null
+	, vlUnitario   Real Not Null
+    , vlFinal      Real	
+	, dataCadastro TimeStamp Not Null Default Now()
+	, Constraint pk_produto_plano_venda_id Primary key(id)
+	, Foreign key (idProduto) References produto(id)
+)With Oids;
+
+Create Or Replace Function func_trig_before_produto_plano_venda_calcular_vlFinal()
+	Returns Trigger As $trig_before_produto_plano_venda_calcular_vlFinal$
+Begin
+	If(Coalesce(New.vlFinal, 0) = 0) Then
+		New.vlFinal = New.quantidade * New.vlUnitario;
+	End If;
+
+	Return New;
+End;
+$trig_before_produto_plano_venda_calcular_vlFinal$ Language 'plpgsql';
+
+
+Create Trigger trig_before_produto_plano_venda_calcular_vlFinal
+	Before Insert Or Update
+	On produto_plano_venda
+	For Each Row
+		Execute Procedure func_trig_before_produto_plano_venda_calcular_vlFinal();
+
+
+
+Create Table cliente_saldo
+(
+	id                        Serial Unique Not Null
+	, idCliente               Int Not Null
+	, saldo                   Real Not Null
+	, dataUltimaMovimentacao  TimeStamp Not Null
+	, Constraint pk_pessoa_saldo_id Primary Key(id)
+	, Foreign Key (idCliente) References cliente(id)
+)With Oids;
+
+
+Create Table cliente_movimentacao_saldo
+(
+	id                Serial Unique Not Null
+	, idClienteSaldo  Int Not Null
+	, idItemVenda     Int Default Null
+	, idTipoMovimento Int Not Null
+	, saldoCreditado  Real Default 0
+	, saldoDebitado   Real Default 0
+	, saldoFinal      Real Not Null
+	, saldoAnterior   Real Not Null
+	, data            TimeStamp Not Null Default Now()
+	, Constraint pk_cliente_movimentacao_saldo_id Primary Key (id)
+	, Foreign Key (idClienteSaldo) References cliente_saldo(id)
+	, Foreign key (idItemVenda) References item_venda(id)
+	, Foreign key (idTipoMovimento) References tipo_movimento(id)
+)With Oids;
+
+
+Create Table sms_lote
+(
+	id                    Serial Unique Not Null
+	, ativo               SmallInt Not Null Default 1
+	, processado          SmallInt Not Null Default 0
+	, idClienteRemetente  Int Not Null
+	, qtdSms              Int Not Null
+	, qtdEnviado          Int Default Null
+	, qtdErro             Int Default Null
+	, status              SmallInt Not Null
+	, envioAgendado       SmallInt Not Null Default 0
+	, dataCadastro        TimeStamp Not Null Default Now ()
+	, dataAgendada        TimeStamp Default Null
+	, dataProcessamento   TimeStamp Default Null
+	, Constraint pk_sms_lote_id Primary key (id)
+	, Foreign key (idClienteRemetente) References cliente(id)
+	, Constraint ck_sms_lote_status Check (status In (1, 2, 3))
+)With Oids;
+Comment On Column sms_lote.status Is 'Status: 1-Não processado; 2-Processamento iniciado; 3-Processado';
+Comment On Column sms_lote.dataAgendada Is 'Refere-se à data no qual o envio deverá ser executado';
+Create Index idx_sms_lote_dataAgendada On sms_lote(dataAgendada);
+
+
+Create Table sms_lote_detalhe
+(
+	id                         Serial Unique Not Null
+	, ativo                    SmallInt Not Null Default 1
+	, idSmsLote                Int Not Null
+	, idPessoaDestinatario     Int Not Null
+	, idPessoaTelefone         Int Not Null
+	, mensagem                 Varchar(160) Not Null
+	, status                   SmallInt Not Null
+	, retorno                  Text
+	, dataCadastro             TimeStamp Not Null Default Now()
+	, dataRetorno              TimeStamp Default Null
+	, Constraint pk_sms_lote_detalhe_id Primary key(id)
+	, Foreign key (idSmsLote) References sms_lote(id)
+	, Foreign key (idPessoaDestinatario) References pessoa(id)
+	, Foreign key (idPessoaTelefone) References pessoa_telefone(id)
+	, Constraint ck_sms_lote_detalhe_status Check (status In (1, 2, 3))
+)With Oids;
+Comment On Column sms_lote_detalhe.status Is 'Status: 1-Não processado; 2-Processamento iniciado; 3-Processado';
+Create Index idx_sms_lote_detalhe On sms_lote_detalhe(idSmsLote);
+
+
+Create Table tag_template
+(
+	id                  Serial Unique Not Null
+	, nome              Varchar(20) Not Null
+	, campoReferencia   Varchar(40) Not Null
+	, Constraint pk_tag_template_id Primary key (id)
+)With Oids;
+Comment on table tag_template Is 'Guarda as tags que podem ser utilizadas para substituir campos padrões. Por exemplo: $(nome), $(telefone), $(celular';
+
+
+Create Table template_sms
+(
+	id              Serial Unique Not Null
+	, ativo         SmallInt Not Null Default 1
+	, global        SmallInt Not Null Default 0
+	, idCliente     Int Default Null
+	, nome          Varchar(100) Not Null
+	, conteudo      Varchar(160) Not Null
+	, dataCadastro  TimeStamp Not Null Default Now()
+	, Constraint pk_template_sms_id Primary Key (id)
+	, Foreign key (idCliente) References cliente(id)
+)With Oids;
+
+
+Create Table serv_servidor_sms
+(
+	id                Serial Unique Not Null
+	, ativo           SmallInt Not Null Default 1
+	, nome            Varchar(40) Not Null
+	, site            Varchar(80)
+	, urlWebService   Varchar(80)
+	, usuario         Varchar(80) Not Null
+	, senha           Varchar(80) Not Null
+	, dataCadastro    TimeStamp Not Null Default Now()
+	, dataDesativacao TimeStamp Default Null
+	, Constraint pk_serv_servidor_sms_id Primary Key(id)
+)With Oids;
+
+Create Table serv_servidor_sms_saldo
+(
+	id                Serial Unique Not Null
+	, idServidorSMS   Int Not Null
+	, saldo           Real Not Null
+	, Constraint pk_serv_servidor_sms_saldo Primary key(id)
+	, Foreign key (idServidorSMS) References serv_servidor_sms(id)
+)With Oids;
---
+++ Scripts para teste.sql
@@ -0,0 +1,182 @@
+﻿Insert Into produto (nome, estoque, vlCustoUnitario, vlVendaUnitario, codigovendavel) Values ('Crédito', 0, 0.02, 0.00, 1);
+
+/* Ao executar o script abaixo, trocar o código do produto */
+Insert Into produto_plano_venda (idProduto, descricao, quantidade, vlUnitario) Values
+(2, '00008 moedas', 8, 0.25)
+, (2, '00050 moedas', 50, 0.20)
+, (2, '00100 moedas', 100, 0.18)
+, (2, '00200 moedas', 200, 0.16)
+, (2, '00300 moedas', 300, 0.14)
+, (2, '00400 moedas', 400, 0.13)
+, (2, '00500 moedas', 500, 0.13)
+, (2, '00800 moedas', 800, 0.12)
+, (2, '01000 moedas', 1000, 0.11)
+, (2, '05000 moedas', 5000, 0.10)
+, (2, '10000 moedas', 10000, 0.09)
+, (2, '15000 moedas', 15000, 0.08)
+, (2, '30000 moedas', 30000, 0.06);
+
+/*
+ Num. moedas     Vl unit     Vl tot
+
+00008 moedas  -  R$ 0.25  -  R$ 2.00
+00050 moedas  -  R$ 0.20  -  R$ 10.00
+00100 moedas  -  R$ 0.18  -  R$ 18.00
+00200 moedas  -  R$ 0.16  -  R$ 32.00
+00300 moedas  -  R$ 0.14  -  R$ 42.00
+00400 moedas  -  R$ 0.13  -  R$ 52.00
+00500 moedas  -  R$ 0.13  -  R$ 65.00
+00800 moedas  -  R$ 0.12  -  R$ 96.00
+01000 moedas  -  R$ 0.11  -  R$ 110.00
+05000 moedas  -  R$ 0.10  -  R$ 500.00
+10000 moedas  -  R$ 0.09  -  R$ 900.00
+15000 moedas  -  R$ 0.08  -  R$ 1200.00
+30000 moedas  -  R$ 0.06  -  R$ 1800.00
+*/
+
+Insert Into template_sms (global, nome, conteudo) Values (1, '$(nome)', 'pessoa.nome');
+
+
+
+Insert Into Pessoa
+(               
+    nome       
+	, sexo                
+	, email               
+	, login               
+	, senha               
+	, tipoPessoa          
+	, rg                 
+	, cpf               
+	, dataNascimento      
+	, usuarioSistema
+	, primeironome
+)
+Values
+(
+     'Gabriel Silveira da Rosa'      
+	, 'M'                
+	, 'gabriel_silveira@hotmail.com'               
+	, 'gabriel'               
+	, '102030'               
+	, 'F'          
+	, '9109235858'                 
+	, '02879452360'               
+	, '1992-10-05'      
+	, 1
+	, 'Gabriel'
+);
+
+
+Insert Into Cliente
+(                  
+	idPessoa       
+	,licencaLiberada
+	,avisoSistema
+	,chaveCripto   
+)
+Values
+(        
+	(Select Max(id) From pessoa)       
+	,1
+	,'Nenhum aviso'   
+	,MD5(Random()::Text)  
+);
+
+Insert Into Pessoa
+(               
+    nome       
+	, sexo                
+	, email               
+	, login               
+	, senha               
+	, tipoPessoa          
+	, rg                 
+	, cpf               
+	, dataNascimento      
+	, usuarioSistema
+	, primeironome
+)
+Values
+(
+     'Jessica dos Santos Conceição'      
+	, 'F'                
+	, 'jessica@bol.com'               
+	, 'jessika'               
+	, 'abcd'               
+	, 'F'          
+	, '505050'                 
+	, '46489023211'               
+	, '1991-08-08'      
+	, 1
+	, 'Jessica'
+);
+
+
+
+Insert Into operadora
+(
+	nome         
+	, Descricao    
+)
+Values
+('Claro', '')
+, ('OI', '')
+, ('Tim', '')
+, ('Vivo', '');
+
+
+Insert Into pessoa_telefone
+(
+    idPessoa       
+	, idTipoTelefone 
+	, idOperadora    
+	, ddi            
+	, ddd            
+	, numero         
+	, observacao
+)
+Values
+(
+	(Select Max(id) From pessoa)
+	, 2
+	, (Select Max(id) from operadora)
+	, '55'
+	, '53'
+	, '984074070'
+	, 'Celular de teste'
+),
+(
+	(Select Max(id) From pessoa)
+	, 3
+	, (Select Max(id) from operadora)
+	, '55'
+	, '53'
+	, '999235688'
+	, 'Outro celular de teste'
+);
+
+Insert Into sms_lote_detalhe
+(
+	, idSmsLote 
+	, idPessoaDestinatario 
+	, idPessoaTelefone 
+	, mensagem 
+	, status 
+)
+Values
+(
+	(Select Max(id) From sms_lote) 
+	, 5 
+	, 6 
+	, 'Mensagem 1' 
+	, 1 
+),
+(
+	(Select Max(id) From sms_lote) 
+	, 5 
+	, 6 
+	, 'Mensagem 2' 
+	, 1 
+);
+
